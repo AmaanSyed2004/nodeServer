53ac19ab8f7bc9648faf349d3c23ec02b80041b3
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-06-26 12:49:41 +0530

    added JWT auth check

--- backend/controllers/protected.js
+++ backend/controllers/protected.js
@@ -1,4 +1,13 @@
-const protected= (req,res)=>{
-  res.json({message: 'This is a protected route, uses middleware to verify token'});
+const User= require('../models/user')
+const protected= async (req,res)=>{
+  try{
+    console.log(req.user)
+    const user = await User.findOne({id: req.user.userID}).select('-password');
+    if (!user) res.status(404).json({message: "User Not found"});
+    res.json({isAuth: true, user});
+  }
+  catch(error){
+    res.sendStatus(500);
+  }
 }
 module.exports=protected
