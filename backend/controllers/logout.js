897cfa25eab501c14b84d4d39bbc9a791436c02f
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-07-08 18:32:50 +0530

    feat: complete logout route

--- backend/controllers/logout.js
+++ backend/controllers/logout.js
@@ -1,4 +1,12 @@
-const logout=()=>{
-
-}
-module.exports= logout
+const logout = (req, res) => {
+  try {
+    if (!req.cookies.authToken){
+        return res.status(200).json({message: "No Cookie found"})
+    }
+    res.clearCookie("authToken");
+    res.status(200).json({ message: "Logged out successfully!" });
+  } catch (e) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+module.exports = logout;
