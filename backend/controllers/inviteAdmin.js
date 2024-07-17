c35010fd3126ca36c67cd2b83e1afc8e77f99048
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-07-17 21:21:07 +0530

    fix: minor return status

--- backend/controllers/inviteAdmin.js
+++ backend/controllers/inviteAdmin.js
@@ -14,9 +14,9 @@ const inviteAdmin= async(req,res)=>{
     await invite.save();
     try {
         await inviteAdminMail({email,token})
-        res.status(200).json({message: 'Invitation has been sent'})
+        return res.status(200).json({message: 'Invitation has been sent'})
     } catch (error) {
-        res.status(500).json({mesage: 'Internal Server error'})
+        return res.status(500).json({mesage: 'Internal Server error'})
     }
 }
 
