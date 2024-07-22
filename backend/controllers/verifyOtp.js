31cb974464548f2f99f334d1a41e6c7e687eb8a1
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-07-22 20:51:16 +0530

    fix: remove redundant return & add info

--- backend/controllers/verifyOtp.js
+++ backend/controllers/verifyOtp.js
@@ -6,22 +6,23 @@ const verifyOTP = async (req, res) => {
   if (!email || !otp) {
     return res.status(400).json({ message: "Email and OTP required" });
   }
-  const entry = await OTP.findOne({ email});
+  const entry = await OTP.findOne({ email });
   const user = await User.findOne({ email });
   if (!entry) {
-    return res.status(404).json({ message: "Email not found" });
+    return res
+      .status(404)
+      .json({
+        message: "Account not found, please enter correct email or register.",
+      });
   }
-  if (!(+entry.otp===+otp)){
-    return res.status(403).json({message: "Invalid OTP"})
-  }
-  if (user.verified){
-    res.status(200).json({message: "Email is already verified"})
+  if (!(+entry.otp === +otp)) {
+    return res.status(403).json({ message: "Invalid OTP" });
   }
 
   //User is OTP verified, update the 'verified' instance in user schema
   user.verified = true;
 
-  //spent an hour debugging why isnt the verified field updating and realised i didnt save. Nice 
+  //spent an hour debugging why isnt the verified field updating and realised i didnt save. Nice
   await user.save();
   res.status(200).json({ message: "OTP Verified!" });
 };
