b4c9815e74124fd2989a03098c45cd9a910363c3
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-07-23 21:36:01 +0530

    minor status fix

--- backend/middleware/rolemiddleware.js
+++ backend/middleware/rolemiddleware.js
@@ -3,7 +3,7 @@ const jwt= require('jsonwebtoken')
 //plan: use jwt to get info about the user attempting to send the invitation 
 const verifyUser = (req, res, next) => {
   jwt.verify(req.cookies.authToken, process.env.ACCESS_SECRET, (err,user)=>{
-    if (err) return res.status(401).json({message: 'No token, Please login first'})
+    if (err) return res.status(404).json({message: 'No token, Please login first'})
     if (!user.role==='client-user') return res.status(403).json({message: 'Invalid role, try an account with super admin role'})
     return next();
   })
@@ -11,7 +11,7 @@ const verifyUser = (req, res, next) => {
 
 const verifyAdmin = (req, res, next) => {
   jwt.verify(req.cookies.authToken, process.env.ACCESS_SECRET, (err,user)=>{
-    if (err) return res.status(401).json({message: 'No token, Please login first'})
+    if (err) return res.status(404).json({message: 'No token, Please login first'})
     if (!user.role==='client-admin') return res.status(403).json({message: 'Invalid role, try an account with super admin role'})
     return next();
   })
@@ -19,7 +19,7 @@ const verifyAdmin = (req, res, next) => {
 
 const verifySuper= (req,res,next)=>{
   jwt.verify(req.cookies.authToken, process.env.ACCESS_SECRET, (err,user)=>{
-    if (err) return res.status(401).json({message: 'No token, Please login first'})
+    if (err) return res.status(404).json({message: 'No token, Please login first'})
     if (!user.role==='super-admin') return res.status(403).json({message: 'Invalid role, try an account with super admin role'})
     return next();
   })
